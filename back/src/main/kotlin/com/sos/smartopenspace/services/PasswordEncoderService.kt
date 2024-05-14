e0b34575e1653c31d87ef235bb2b0c999bb351c2
Jose Cassano <cassanojoseluis@gmail.com>
2024-05-14 09:37:01 -0300

    delete autowired annotation because its default

--- back/src/main/kotlin/com/sos/smartopenspace/services/PasswordEncoderService.kt
+++ back/src/main/kotlin/com/sos/smartopenspace/services/PasswordEncoderService.kt
@@ -1,13 +1,11 @@
 package com.sos.smartopenspace.services
 
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
 
 @Service
 class PasswordEncoderService(
-    @Autowired
     private val passwordEncoder: PasswordEncoder = BCryptPasswordEncoder()
 ) {
 
